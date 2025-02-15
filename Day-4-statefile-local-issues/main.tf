2c2afcbe11ac45831dd3600c0b230d0dd1ab60b2
Uday <puday0730@gmail.com>
2025-02-16 01:05:57 +0530

    dodified

--- Day-4-statefile-local-issues/main.tf
+++ Day-4-statefile-local-issues/main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "name" {
-    ami = "ami-085ad6ae776d8f09c"
+    ami = "ami-0ddfba243cbee3768"
     instance_type = "t2.micro"
-    key_name = "ec2test"
-    availability_zone = "us-east-1a"
-    tags = {
-      Name = "dev"
+    key_name = "2301"
+    
+
     }
    
   
-}
+
 
 
 #note Developers Overwriting Each Other’s Work if separe state file for same project
