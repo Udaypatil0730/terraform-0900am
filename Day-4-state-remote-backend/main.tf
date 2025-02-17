3823b7bd6e827bf3ba70ced0ec25482fa7d90ad8
Uday <puday0730@gmail.com>
2025-02-17 14:07:25 +0530

    modified prod instance

--- Day-4-state-remote-backend/main.tf
+++ Day-4-state-remote-backend/main.tf
@@ -2,13 +2,23 @@ resource "aws_instance" "name" {
     ami = "ami-0ddfba243cbee3768"
     instance_type = "t2.micro"
     key_name = "2301"
-    #availability_zone = "us-east-1a"
-    #tags = {
-    #Name = "dev"
+    tags = {
+    Name = "dev"
     }
    
   
+}
 
+resource "aws_instance" "prod" {
+    ami = "ami-0ddfba243cbee3768"
+    instance_type = "t2.nano"
+    key_name = "2301"
+    tags = {
+    Name = "prod"
+    }
+   
+  
+}
 
 #Note:When multiple developers work on the same Terraform state file (terraform.tfstate), several problems can arise. These issues can cause conflicts, resource corruption, and deployment failures.
 #Merge Conflicts in terraform.tfstate
