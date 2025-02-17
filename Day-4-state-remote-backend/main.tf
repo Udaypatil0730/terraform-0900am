fd8c7852ff81a39b2e20f10a7bc789423099a836
Uday <puday0730@gmail.com>
2025-02-17 17:01:36 +0530

    added one more server

--- Day-4-state-remote-backend/main.tf
+++ Day-4-state-remote-backend/main.tf
@@ -20,6 +20,15 @@ resource "aws_instance" "prod" {
   
 }
 
+resource "aws_instance" "spyder" {
+    ami = "ami-0ddfba243cbee3768"
+    instance_type = "t2.micro"
+    key_name = "2301"
+    tags = {
+    Name = "spyder"
+    }
+
+}
 #Note:When multiple developers work on the same Terraform state file (terraform.tfstate), several problems can arise. These issues can cause conflicts, resource corruption, and deployment failures.
 #Merge Conflicts in terraform.tfstate
 #If a developer runs terraform plan on an outdated state file, and another applies changes, the state will be out of sync.
